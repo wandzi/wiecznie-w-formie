4652f646a6d4beac7b5626f7b107c763b6bcac12
wandzi <wandzel.mateusz@yahoo.com>
2020-08-02 18:02:28 +0200

    Feat(Video): Added filter and button to the video.

--- src/components/Video/Video.js
+++ src/components/Video/Video.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "gatsby"
 
 import video from '../../video/background_movie.mp4'
 import './Video.scss'
@@ -8,6 +9,8 @@ const Video = () => (
         <video className="background-video"  autoPlay loop muted>
             <source src={video} type='video/mp4' />
         </video>
+        <Link to="/o-mnie" className="video-button">Poznaj mnie...</Link>
+        <div className="filter"></div>
     </>
 )
 
